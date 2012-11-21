8bff0ae90244142de39447757c271ea06cbc9cc5
Damien Papworth <damien.papworth@vibrantmedia.com>
2012-11-21 10:04:00 -0500

    Initial commit.

---
+++ moduleC/src/test/javascript/IncludesTransitiveDependencySpec.js
@@ -0,0 +1,8 @@
+describe('IncludesTransitiveDependency',function(){
+
+    it('should say hello',function(){
+        var helloWorld = new HelloWorld();
+        expect(helloWorld.greeting()).toBe("Hello, World");
+    });
+
+});
